dc1dda8d1032c7f8d9648b53130c55c65d2aaeb4
funnythingz <funnythingz69+github@gmail.com>
2016-02-27 23:47:10 +0900

    Layout

--- app/src/main/java/com/funnythingz/pokedexandroid/AppActivity.java
+++ app/src/main/java/com/funnythingz/pokedexandroid/AppActivity.java
@@ -2,8 +2,14 @@ package com.funnythingz.pokedexandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import butterknife.Bind;
 
 public class AppActivity extends AppCompatActivity {
+
+    @Bind(R.id.pokemon_list_view)
+    ListView pokemonListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
