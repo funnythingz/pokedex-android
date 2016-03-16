bf5e44c3f40460d491360584c5621cdc9cb90b3e
funnythingz <funnythingz69+github@gmail.com>
2016-03-16 13:48:29 +0900

    show logging of Retrofit

--- app/src/main/java/com/funnythingz/pokedexandroid/domain/PokemonRepository.java
+++ app/src/main/java/com/funnythingz/pokedexandroid/domain/PokemonRepository.java
@@ -4,6 +4,8 @@ import com.funnythingz.pokedexandroid.infra.PokemonAPI;
 
 import java.util.List;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -11,27 +13,35 @@ import rx.Observable;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
 
+import static okhttp3.logging.HttpLoggingInterceptor.Level;
+
 public class PokemonRepository {
 
     final String ENDPOINT = "http://pokeapi.co/api/v2/";
 
     private static final PokemonRepository pokemonRepository = new PokemonRepository();
 
+    private PokemonAPI pokemonAPI;
+
     private PokemonRepository() {
-        // No instances.
+        pokemonAPI =  new Retrofit.Builder()
+                .baseUrl(ENDPOINT)
+                .client(
+                        new OkHttpClient()
+                                .newBuilder()
+                                .addInterceptor(new HttpLoggingInterceptor().setLevel(Level.BODY))
+                                .build()
+                )
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .build()
+                .create(PokemonAPI.class);
     }
 
     public static PokemonRepository getInstance() {
         return pokemonRepository;
     }
 
-
-    private PokemonAPI pokemonAPI = new Retrofit.Builder()
-            .baseUrl(ENDPOINT)
-            .addConverterFactory(GsonConverterFactory.create())
-            .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
-            .build()
-            .create(PokemonAPI.class);
 
     public Observable<List<Pokemon>> fetchPokemonList() {
         return pokemonAPI.getPokemonResponseDataList()
--- app/src/main/java/com/funnythingz/pokedexandroid/presentation/PokemonActivity.java
+++ app/src/main/java/com/funnythingz/pokedexandroid/presentation/PokemonActivity.java
@@ -1,6 +1,5 @@
 package com.funnythingz.pokedexandroid.presentation;
 
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
